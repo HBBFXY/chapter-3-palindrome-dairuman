100be17bf4a237270f9c72977554dfbe36da919b
dairuman <2419974912@qq.com>
2025-09-17 12:13:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
- num = int(input("请输入一个五位数："))
- ten_thousand = num // 10000
- thousand = (num % 10000) // 1000
- hundred = (num % 1000) // 100
- ten = (num % 100) // 10
- unit = num % 10
- # 判断是否为回文数：万位和个位相等，且千位和十位相等
- if ten_thousand == unit and thousand == ten:
-     print("是回文数")
- else:
-     print("不是回文数")
+ n = input('请输入一个5位数字: ')
+if len(n) != 5 or not n.isdigit():
+    print('输入错误: 请输入5位数字')
+else:
+    if n == n[::-1]:
+        print('是回文数')
+    else:
+        print('不是回文数')
